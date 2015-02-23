644a2e3283aee62d2c2110d0f0c31468ce4e493a
dcolclazier <david@jdcnetworksolutions.com>
2015-02-22 23:12:17 -0700

    some debugging

--- Assets/Code/Scripts/PathTest.cs
+++ Assets/Code/Scripts/PathTest.cs
@@ -27,18 +27,19 @@ public class PathTest : MonoBehaviour
     {
         var lineRenderer = goFrom.GetComponent<LineRenderer>();
 
-        var angleToDest = Vector3.Angle(goFrom.transform.position, to);
-        
+        var angleToDest = Vector2.Angle(goFrom.transform.position, to);
+        Debug.Log("Angle: " + angleToDest);
+
 
         lineRenderer.SetVertexCount(2);
         lineRenderer.SetWidth(.05f,.05f);
         lineRenderer.SetColors(color,color);
         lineRenderer.SetPosition(0,goFrom.transform.position);
         lineRenderer.SetPosition(1,PointOnCircle(.25f,angleToDest,to));
-        DrawCircle(goFrom, to,1);
+        DrawCircle(to,1);
     }
 
-    public void DrawCircle(GameObject go, Vector3 center, float radius)
+    public void DrawCircle(Vector3 center, float radius)
     {
         _circleRenderer.SetVertexCount(126); 
         _circleRenderer.SetColors(Color.white,Color.white);
